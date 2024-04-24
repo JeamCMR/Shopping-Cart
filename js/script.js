faefd7b6413068e5eb209d98dd7e2f7f3e2b8cc4
COPSTA <tutomx16@gmail.com>
2024-04-24 17:06:28 -0500

    Agrega evento para mostrar el carrito de compras

--- js/script.js
+++ js/script.js
@@ -147,3 +147,8 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
     }
 );
 
+cartBtn.addEventListener("click", ()=>{
+    isCartShowing = !isCartShowing;
+    showHideCartSpan.textContent =  isCartShowing? "Hide" : "Show";
+    cartContainer.style.display = isCartShowing? "block" : "none";
+})

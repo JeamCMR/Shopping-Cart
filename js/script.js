c4cab71ce32e9c333676e2b603db8be37cb92327
COPSTA <tutomx16@gmail.com>
2024-04-24 13:53:28 -0500

    Agrega declaracion de variables de elementos del DOM necesarios

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ const dessertCards = document.getElementById("dessert-card-container");
 //Botones
 const cartBtn = document.getElementById("cart-btn");
 const clearCartBtn = document.getElementById("clear-cart-btn");
+
+//Totales
+const totalNumberOfItems = document.getElementById("total-items");
+const cartSubTotal = document.getElementById("subtotal");
+const cartTaxes = document.getElementById("taxes");
+const cartTotal = document.getElementById("total");
+
+// Cuadro de dialogoDialogo
+const showHideCartSpan = document.getElementById("show-hide-cart");
+let isCartShowing = false;

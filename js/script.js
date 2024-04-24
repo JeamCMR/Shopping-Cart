40088d4232ffda8fcc3ac18d73cb5d6d375e026f
COPSTA <tutomx16@gmail.com>
2024-04-24 14:37:43 -0500

    Añade forEach el cual recorre el array products y visualiza en el html los productos

--- js/script.js
+++ js/script.js
@@ -93,3 +93,17 @@ const products = [
     }
 ];
 
+
+//Visualizar productos en el html
+products.forEach(
+    ({name,id,price,category})=>{
+        dessertCards.innerHTML +=`
+        <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button id="${id}" class="btn add-to-cart-btn">Add to cart</button>
+        </div>
+        `
+    }
+    );

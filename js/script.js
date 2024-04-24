1ff5bb3a66332cfe9c85986e3f2c166610b0c005
COPSTA <tutomx16@gmail.com>
2024-04-24 16:31:28 -0500

    Agrega la clase ShoppingCart, sus propiedades y metodos

--- js/script.js
+++ js/script.js
@@ -107,3 +107,30 @@ products.forEach(
         `
     }
     );
+
+class ShoppingCart {
+    constructor(){
+        this.items = [];
+        this.total = 0;
+        this.taxRate = 8.25;
+    }
+    addItem(id,products) {
+        const product = products.find((item)=> item.id === id);
+        const { name , price } = product;
+        this.items.push(product);
+        const totalCountPerProduct = {};
+        this.items.forEach(dessert => {
+            totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+        });
+        const currentProductCount = totalCountPerProduct[product.id];
+        const currentProductCountSpan = document.getElementById(`product-count-for-id${product.id}`);
+        currentProductCount > 1 ? currentProductCountSpan.textContent = `${currentProductCount}x` 
+        : productsContainer.innerHTML += `
+        <div class="product" id="dessert${id}"> 
+        <p><span class="product-count" id="product-count-for-id${id}"></span>${name}</p>
+        <p>${price}</p>
+        </div>` 
+    }
+    
+};
+

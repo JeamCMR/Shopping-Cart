3187dced7557e8def18894ccecfe46c7e3247c8d
COPSTA <tutomx16@gmail.com>
2024-04-24 13:43:35 -0500

    Declara y inicializa variables del DOM

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/**Variables */
+
+const cartContainer = document.getElementById("cart-container");
+const productsContainer = document.getElementById("products-container");
+const dessertCards = document.getElementById("dessert-card-container");
+//Botones
+const cartBtn = document.getElementById("cart-btn");
+const clearCartBtn = document.getElementById("clear-cart-btn");

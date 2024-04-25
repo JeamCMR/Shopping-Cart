90c6f92f6c02bb7a1b2cbf93e2857c147d0d5ce8
COPSTA <tutomx16@gmail.com>
2024-04-24 22:42:56 -0500

    Añade mas calculos al metodo calculateTotal

--- js/script.js
+++ js/script.js
@@ -137,8 +137,18 @@ class ShoppingCart {
       }
     
       calculateTotal(){
-        const subTotal = this.items.reduce((total,item) => total + item.price,0 )
+        const subTotal = this.items.reduce((total,item) => total + item.price,0 );
+        const tax = this.calculateTaxes(subTotal);
+        this.total = subTotal + tax;
+        cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+        cartTaxes.textContent = `$${tax.toFixed(2)}`;
+        cartTotal.textContent = `$${this.total.toFixed(2)}`;
+        return this.total;
       }
+
+    calculateTaxes(amount){
+        return parseFloat(((this.taxRate/100) * amount).toFixed(2));
+    }
 };
 
 
@@ -151,6 +161,7 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
         btn.addEventListener("click", event =>{
             cart.addItem(Number(event.target.id),products);
             totalNumberOfItems.textContent = cart.getCounts();
+            cart.calculateTotal()
         })
     }
 );
